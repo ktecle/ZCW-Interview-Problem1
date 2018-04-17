2b4e5c927e322cb1ca7a496b4ac2aa329ccc801c
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-04-16 21:17:30 -0400

    both working

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+
+    public String replaceValues(String input) {
+        Map<Character,Character> myMap = generateMap();
+
+        char[] myChars = input.toCharArray();
+        for(int i =0;i<myChars.length;i++){
+            if(myMap.containsKey((Character.toLowerCase(myChars[i])))){
+                myChars[i]=myMap.get(Character.toLowerCase(myChars[i]));
+            }
+        }
+       return  printCharacters(myChars);
+    }
+
+    public String printCharacters(char[] input){
+        String result = "";
+        for(int i =0;i<input.length;i++){
+            result+=input[i];
+        }
+        return result;
+    }
+
+    public Map<Character,Character> generateMap(){
+        Map<Character,Character> myMap = new HashMap<Character,Character>();
+        myMap.put('f','7');
+        myMap.put('s','$');
+        myMap.put('1','!');
+        myMap.put('a','@');
+
+        return myMap;
+    }
+
+    public String replaceValuesRecursive(String input, int index){
+        Map<Character,Character> myMap = generateMap();
+        char[] myChars = input.toCharArray();
+        if(myMap.containsKey(Character.toLowerCase(myChars[index]))){
+            myChars[index]=myMap.get(Character.toLowerCase(myChars[index]));
+        }
+        if(index<input.length()-1){
+            index++;
+            return replaceValuesRecursive(new String(myChars),index);
+
+        }
+        return new String(myChars);
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void testReplaceValues1(){
+        Problem1 test = new Problem1();
+        String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+        String actual  = test.replaceValues(input);
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testReplaceValues2(){
+        Problem1 test = new Problem1();
+        String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+        String actual  = test.replaceValuesRecursive(input,0);
+        Assert.assertEquals(expected,actual);
+
+    }
+
 }
